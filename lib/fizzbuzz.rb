16981ff266dd58529539ac2c670d95c01ec097d7
Aiden <aidengleave@gmail.com>
2018-12-16 10:04:45 +0000

    change to % 15

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(number)
-  if number % 15 == 0
+  if number % 3 == 0 && number % 5 == 0
     'fizzbuzz'
-  elsif number % 5 == 0 
+  elsif number % 5 == 0
     'buzz'
-  elsif number % 3 == 0 
+  elsif number % 3 == 0
     'fizz'
   else
     number
